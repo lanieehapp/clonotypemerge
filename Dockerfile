222554b78ce984abf3dcf9045e12840dfdfe1548
lanieehapp <lanieehapp@gmail.com>
2021-06-23 12:11:00 -0700

    adding merger script and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# Base Image
+FROM r-base:4.0.2
+
+# Maintainer
+MAINTAINER DaveLab <lab.dave@gmail.com>
+
+# update the OS related packages
+RUN apt-get update -y && apt-get install -y \
+    build-essential \
+    vim \
+    less \
+    gawk \
+    git \
+    libcurl4-gnutls-dev \
+    libxml2-dev \
+    libssl-dev \
+    wget \
+    bedtools \
+    parallel \
+    unzip \
+    cmake \
+    libxml2-dev  	
+
+WORKDIR /usr/local/bin
+
+# clone clonotypemerge repo
+ADD https://api.github.com/repos/lanieehapp/clonotypemerge/git/refs/heads/ version.json
+RUN git clone https://github.com/lanieehapp/clonotypemerge.git
+
+# add clonotypemerge repo to SYSPATH
+ENV PATH clonotypemerge:$PATH
+
+# change the permission of the repo
+RUN chmod 777 -R variantmerge
+WORKDIR /usr/local/bin/clonotypemerge
---
+++ clonotype_merge.R
@@ -0,0 +1,37 @@
+####merge clonotype files####
+
+
+args = commandArgs(trailingOnly=TRUE)
+
+input.dir<-args[[1]]
+
+all.ig<-list.files(path=input.dir, pattern="*IG.all.clones*", full.names = TRUE)
+top.ig<-list.files(path=input.dir, pattern="*IG.top.clones*", full.names = TRUE)
+all.t<-list.files(path=input.dir, pattern="*T.all.clones*", full.names = TRUE)
+top.t<-list.files(path=input.dir, pattern="*T.top.clones*", full.names = TRUE)
+
+all.ig.merged<-NULL
+top.ig.merged<-NULL
+all.t.merged<-NULL
+top.t.merged<-NULL
+
+for( i in 1:length(filt.file.list)){
+  
+  print(i)
+  curr.samp<-read.delim(all.ig[i], header=TRUE, sep="\t", check.names = FALSE)
+  all.ig.merged<-rbind(all.ig.merged, curr.samp)
+  
+  curr.samp<-read.delim(top.ig[i], header=TRUE, sep="\t", check.names = FALSE)
+  top.ig.merged<-rbind(top.ig.merged, curr.samp)
+  
+  curr.samp<-read.delim(all.t[i], header=TRUE, sep="\t", check.names = FALSE)
+  all.t.merged<-rbind(all.t.merged, curr.samp)
+  
+  curr.samp<-read.delim(top.t[i], header=TRUE, sep="\t", check.names = FALSE)
+  top.t.merged<-rbind(top.t.merged, curr.samp)
+}
+
+write.table(all.ig.merged, file=args[2], row.names = FALSE, quote = FALSE, sep="\t")
+write.table(top.ig.merged, file=args[3], row.names = FALSE, quote = FALSE, sep="\t")
+write.table(all.t.merged, file=args[4], row.names = FALSE, quote = FALSE, sep="\t")
+write.table(top.t.merged, file=args[5], row.names = FALSE, quote = FALSE, sep="\t")
